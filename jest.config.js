31a162161f32924d496c64139642baf64bdd2312
DNature <divinehycenth@outlook.com>
2020-01-25 16:12:01 +0100

    Graphql Yoga and jest working together

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+const { defaults } = require('jest-config');
+
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  verbose: true,
+  moduleFileExtensions: [
+    ...defaults.moduleFileExtensions,
+    'ts',
+    'tsx',
+    'js',
+    'jsx',
+    'json',
+    'node'
+  ]
+};
