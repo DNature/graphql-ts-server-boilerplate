ed95032d82c3a44025cc88bebbd1547b9b3424f8
DNature <divinehycenth@outlook.com>
2020-01-25 17:01:57 +0100

    graphql schema stitching

--- src/startServer.ts
+++ src/startServer.ts
@@ -1,13 +1,23 @@
 import { GraphQLServer } from 'graphql-yoga';
 import { importSchema } from 'graphql-import';
-import { resolvers } from './modules/register/resolvers';
 import * as path from 'path';
+import { readdirSync } from 'fs';
 import { createTypeormConn } from './utils/createTypormConn';
+import { mergeSchemas, makeExecutableSchema } from 'graphql-tools';
+import { GraphQLSchema } from 'graphql';
 
 export const startServer = async () => {
-  const typeDefs = importSchema(path.join(__dirname, 'schema.graphql'));
+  const schemas: GraphQLSchema[] = [];
+  const folders = readdirSync(path.join(__dirname, './modules'));
+  folders.forEach(folder => {
+    const { resolvers } = require(`./modules/${folder}/resolvers`);
+    const typeDefs = importSchema(
+      path.join(__dirname, `./modules/${folder}/schema.graphql`)
+    );
+    schemas.push(makeExecutableSchema({ resolvers, typeDefs }));
+  });
 
-  const server = new GraphQLServer({ typeDefs, resolvers });
+  const server = new GraphQLServer({ schema: mergeSchemas({ schemas }) });
   await createTypeormConn();
   const app = await server.start({
     port: process.env.NODE_ENV === 'test' ? 0 : 4000
